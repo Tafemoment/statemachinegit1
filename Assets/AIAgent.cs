2c6dd05a662c5f5acdd5931e5a2766a994f062ff
slazaryn <luca.kippinghilder@studytafensw.edu.au>
2022-08-11 13:22:28 +1000

    made angy enemy

--- Assets/AIAgent.cs
+++ Assets/AIAgent.cs
@@ -12,15 +12,53 @@ public class AIAgent : MonoBehaviour
     [SerializeField] private Transform[] _patrolpoints;
     [SerializeField] private int _patrolpointIndex = 0;
 
-    void Update()
+    public void Patrol()
     {
         Vector2 patrolpointPosition = _patrolpoints[_patrolpointIndex].position;
         MoveToPoint(patrolpointPosition);
         //MoveToPoint(_player.transform.position);
-        if (Vector2.Distance(transform.position, patrolpointPosition) < 0.5f)
+        if (Vector2.Distance(transform.position, patrolpointPosition) < 0.1f)
         {
-            _patrolpointIndex = (_patrolpointIndex++)%(_patrolpoints.Length);
+            _patrolpointIndex++; // ++ Increase by 1 integer
         }
+
+        if (_patrolpointIndex >= _patrolpoints.Length)
+        {
+            _patrolpointIndex = 0;
+        }
+    }
+
+    public void Search()
+    {
+        int closestIndex = -1;
+        float closestDistance = float.MaxValue;
+        for(int index = 0; index < _patrolpoints.Length; index++)
+        {
+            float currentDistance = Vector2.Distance(_patrolpoints[index].position, transform.position);
+            if (currentDistance < closestDistance)
+            {
+                closestDistance = currentDistance;
+                closestIndex = index;
+            }
+            
+        }
+        _patrolpointIndex = closestIndex;
+    }
+
+    public bool IsPlayerInRange()
+    {
+        if (Vector2.Distance(transform.position, _player.transform.position) < 7f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void HuntPlayer()
+    {
+        MoveToPoint(_player.transform.position);
     }
     void MoveToPoint(Vector2 point)
     {
--- Assets/StateMachine.cs
+++ Assets/StateMachine.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AIAgent))]
 public class StateMachine : MonoBehaviour
 {
 
@@ -9,16 +10,14 @@ public class StateMachine : MonoBehaviour
     {
         Patrol,
         Hunt,
-        Pursue,
         FindBerries
     }
 
     [SerializeField] private State _state;
 
-    private void Start()
-    {
-        NextState();
-    }
+    private AIAgent _aiAgent;
+    private SpriteRenderer _spriteRenderer;
+
 
     private void NextState()
     {
@@ -30,25 +29,59 @@ public class StateMachine : MonoBehaviour
             case State.FindBerries:
                 StartCoroutine(FindBerriesState());
                 break;
+            case State.Hunt:
+                StartCoroutine(HuntState());
+                break;
             default:
                 Debug.LogWarning("State does not exist in NextState function, stopping statemachine");
                 break;
         }
     }
+
+   
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _aiAgent = GetComponent<AIAgent>();
+
+        NextState();
+    }
     private IEnumerator PatrolState()
     {
         Debug.Log("Patrolling");
+        _aiAgent.Search();
+        _spriteRenderer.color = Color.black;
         while (_state == State.Patrol)
         {
+            _aiAgent.Patrol(); 
+            if (_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Hunt;
+            }
             yield return null; //wait 1 frame
         }
         Debug.Log("Halting Patrol");
         NextState();
     }
-    
+    private IEnumerator HuntState()
+    {
+        Debug.Log("AYO C'MERE");
+        _spriteRenderer.color = new Color(1, 0.3f, 0, 1);
+        while (_state == State.Hunt)
+        {
+            _aiAgent.HuntPlayer();
+            if (!_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Patrol;
+            }
+            yield return null; //wait a single
+        }
+        Debug.Log("Lost em");
+        NextState();
+    }
     private IEnumerator FindBerriesState()
     {
-        Debug.Log("Berry Picking");
+        Debug.Log("mmmmm Berries");
         while (_state == State.FindBerries)
         {
             yield return null; //wait 1 frame
@@ -56,7 +89,4 @@ public class StateMachine : MonoBehaviour
         Debug.Log("Ending Berry Search");
         NextState();
     }
-
-    
-
 }
